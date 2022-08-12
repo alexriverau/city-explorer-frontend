233972dd64fcc05511024bba095079beb17ae9a5
Alejandro Rivera <alexrivera78@gmail.com>
2022-08-11 19:12:49 -0500

    lab07 completed

--- src/Weather.js
+++ src/Weather.js
@@ -3,22 +3,16 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import axios from 'axios';
+import Error from './Error';
 
 class Weather extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       weather: [],
-      error: ''
+      error: '',
     };
   }
-
-  // getWeather = async () => {
-  //   const url = `${process.env.REACT_APP_SERVER}/weather?searchQuery=${this.props.search}`;
-  //   const response = await axios.get(url);
-  //   console.log(response);
-  //   this.setState({ weather: response.data });
-  // };
 
   getWeather = async () => {
     try {
@@ -41,9 +35,8 @@ class Weather extends React.Component {
         <Button onClick={this.getWeather} variant='info' type='submit'>
           Weather
         </Button>
-
         <Container style={{ width: '18rem' }}>
-          <Card className='mt-4'>
+          <Card className='m-4'>
             {this.state.weather.length > 0 &&
               this.state.weather.map((city, idx) => (
                 <Card.Body key={idx}>
@@ -54,6 +47,7 @@ class Weather extends React.Component {
               ))}
           </Card>
         </Container>
+        <Error error={this.state.error} />
       </Container>
     );
   }
